467fca1ed41041e67fdea121f7dac6fa82aba76d
anekos <anekos@snca.net>
2009-03-31 12:41:00 +0900

    自動コメント欄オープンの設定を追加 実装はまだ

--- source/defaults/preferences/ankpixiv.js
+++ source/defaults/preferences/ankpixiv.js
@@ -3,3 +3,4 @@ pref("extensions.ankpixiv.checkExistingDownload", true);
 pref("extensions.ankpixiv.saveHistory", true);
 pref("extensions.ankpixiv.defaultFilename", '?member-name? - ?title?');
 pref("extensions.ankpixiv.largeOnMiddle", true);
+pref("extensions.ankpixiv.openComment", false);
