d4e5fbd957839cd20e2dad3d711efda97f5746a3
anekos <anekos@snca.net>
2009-09-02 20:15:28 +0900

    remove bat file.

--- build/build.bat
+++
@@ -1,9 +0,0 @@
-set name=ank_pixiv_tool-1.x.y
-
-cd C:\root\project\FirefoxAddons\ank_pixiv\trunk\build\
-
-copy %1 ..\source\
-7z a -tzip "%name%.xpi" ..\source\* -x!*.svn -r -mx=9
-sha1sum "%name%.xpi"
-
-PAUSE
