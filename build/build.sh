e6f7b72f75a919afeb13f794a5a72b92069d3f9d
anekos <anekos@snca.net>
2009-10-31 08:34:07 +0900

    最小の locale でビルドできるように

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,18 @@ xpi=ank_pixiv_tool.xpi
 
 cd ../source
 rm ../build/$xpi
-zip -r ../build/$xpi *
+
+case $1 in
+  locale)
+    zip -r ../build/$xpi chrome.manifest defaults document install.rdf chrome/content chrome/locale/en-US chrome/locale/ja-JP
+  ;;
+
+  all)
+    zip -r ../build/$xpi chrome.manifest defaults document install.rdf chrome/content chrome/locale
+  ;;
+  *)
+    echo "all or locale?"
+esac
 
 cd ../build
 
