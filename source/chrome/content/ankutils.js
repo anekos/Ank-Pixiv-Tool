ea726cba85de26e284d4630e864b2f0e6bc4dda1
anekos <anekos@snca.net>
2010-02-06 16:20:10 +0900

    createHTMLDocument 追加

--- source/chrome/content/ankutils.js
+++ source/chrome/content/ankutils.js
@@ -307,6 +307,29 @@ try {
     ********************************************************************************/
 
     /*
+     * createHTMLDocument
+     * http://nanto.asablo.jp/blog/2009/10/29/4660197
+     */
+    createHTMLDocument: function (source) {
+      let wcnt = window.content;
+      let doc = wcnt.document.implementation.createDocument(
+        'http://www.w3.org/1999/xhtml',
+        'html',
+        wcnt.document.implementation.createDocumentType(
+          'html',
+          '-//W3C//DTD HTML 4.01//EN',
+          'http://www.w3.org/TR/html4/strict.dtd'
+        )
+      );
+      let range = wcnt.document.createRange();
+      range.selectNodeContents(wcnt.document.documentElement);
+      let content = doc.adoptNode(range.createContextualFragment(source));
+      doc.documentElement.appendChild(content);
+      return doc;
+    },
+
+
+    /*
      * findNodeByXPath
      *    xpath:
      *    return: node
