573c3715df40568460effa5c91e86a7046493b13
anekos <anekos@snca.net>
2014-02-09 13:48:01 +0900

    fixes #17 - R18 漫画で 3rd パーティークッキーの設定有効にしなくてもすむように
    Thanks dahanc!

--- source/chrome/content/ankutils.js
+++ source/chrome/content/ankutils.js
@@ -426,6 +426,11 @@ try {
      function rfe (callback) {
         let xhr = new XMLHttpRequest();
         xhr.open('HEAD', url, true);
+        try {
+          xhr.channel.QueryInterface(Ci.nsIHttpChannelInternal).forceAllowThirdPartyCookie = true;
+        } catch(ex) {
+          /* unsupported by this version of FF */
+        }
         xhr.onreadystatechange = function (e) {
           if (xhr.readyState == 4)
             callback(xhr.status);
@@ -458,6 +463,11 @@ try {
      let text = null;
      let xhr = new XMLHttpRequest();
      xhr.open((post ? 'POST' : 'GET'), url, false);
+     try {
+       xhr.channel.QueryInterface(Ci.nsIHttpChannelInternal).forceAllowThirdPartyCookie = true;
+     } catch(ex) {
+       /* unsupported by this version of FF */
+     }
      xhr.onreadystatechange = function () {
        if (xhr.readyState == 4 && xhr.status == 200) {
          text = xhr.responseText;
